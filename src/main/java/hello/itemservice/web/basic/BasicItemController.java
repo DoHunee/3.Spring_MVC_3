70d333d90287daec1982d6f86f0f9af3354cb3c9
장도훈 <jehgns1@gmail.com>
2024-08-14 13:31:15 +0900

    상품 등록 처리 = @ModelAttribute

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -5,8 +5,11 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
@@ -45,9 +48,49 @@ class BasicItemController {
         return "basic/addForm";
     }
 
-    /**
-     * 테스트용 데이터 추가
-     */
+    // 상품 등록 처리 1
+    // http://localhost:8080/basic/items/add
+    @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName, @RequestParam int price, @RequestParam Integer quantity, Model model) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+        itemRepository.save(item);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    
+    // 상품 등록 처리 2
+    // @ModelAttribute("item") Item item model.addAttribute("item", item); 자동 추가
+    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+        itemRepository.save(item);
+        //model.addAttribute("item", item); //자동 추가, 생략 가능
+        return "basic/item";
+    }
+
+    
+     // 상품 등록 처리 3
+     // @ModelAttribute name 생략 가능 model.addAttribute(item); 자동 추가, 생략 가능 생략시
+     // model에 저장되는 name은 클래스명 첫글자만 소문자로 등록 Item -> item
+    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    // 상품 등록 처리 4
+    // @ModelAttribute 자체 생략 가능
+    // model.addAttribute(item) 자동 추가
+    @PostMapping("/add")
+    public String addItemV4(Item item) {
+    itemRepository.save(item);
+    return "basic/item";
+    }
+
+    // 테스트용 데이터 추가
     @PostConstruct
     public void init() {
         itemRepository.save(new Item("testA", 10000, 10));
