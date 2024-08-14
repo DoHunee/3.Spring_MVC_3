ea31b67ca746ca94d74aac1aad95abd1586bc08c
장도훈 <jehgns1@gmail.com>
2024-08-14 16:31:40 +0900

    상품수정

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -23,7 +23,7 @@ class BasicItemController {
 
     private final ItemRepository itemRepository;
 
-    // 상품 목록 조회
+    // 상품 목록 조회 폼
     // http://localhost:8080/basic/items
     @GetMapping
     public String items(Model model) {
@@ -32,7 +32,7 @@ class BasicItemController {
         return "basic/items";
     }
 
-    // 상품 상세 조회
+    // 상품 상세 조회 폼
     // http://localhost:8080/basic/items/1  => 이렇게 하면 상품 ID가 1인 상품을 조회한다.
     @GetMapping("/{itemId}")
     public String item(@PathVariable Long itemId, Model model) {
@@ -41,7 +41,7 @@ class BasicItemController {
         return "basic/item";
     }
 
-    // 상품 등록 처리
+    // 상품 등록 폼
     // http://localhost:8080/basic/items/add
     @GetMapping("/add")
     public String addForm() {
@@ -50,7 +50,7 @@ class BasicItemController {
 
     // 상품 등록 처리 1
     // http://localhost:8080/basic/items/add
-    @PostMapping("/add")
+    // @PostMapping("/add")
     public String addItemV1(@RequestParam String itemName, @RequestParam int price, @RequestParam Integer quantity, Model model) {
         Item item = new Item();
         item.setItemName(itemName);
@@ -61,21 +61,19 @@ class BasicItemController {
         return "basic/item";
     }
 
-    
     // 상품 등록 처리 2
     // @ModelAttribute("item") Item item model.addAttribute("item", item); 자동 추가
-    @PostMapping("/add")
+    // @PostMapping("/add")
     public String addItemV2(@ModelAttribute("item") Item item, Model model) {
         itemRepository.save(item);
         //model.addAttribute("item", item); //자동 추가, 생략 가능
         return "basic/item";
     }
 
-    
-     // 상품 등록 처리 3
-     // @ModelAttribute name 생략 가능 model.addAttribute(item); 자동 추가, 생략 가능 생략시
-     // model에 저장되는 name은 클래스명 첫글자만 소문자로 등록 Item -> item
-    @PostMapping("/add")
+    // 상품 등록 처리 3
+    // @ModelAttribute name 생략 가능 model.addAttribute(item); 자동 추가, 생략 가능 생략시
+    // model에 저장되는 name은 클래스명 첫글자만 소문자로 등록 Item -> item
+    // @PostMapping("/add")
     public String addItemV3(@ModelAttribute Item item) {
         itemRepository.save(item);
         return "basic/item";
@@ -86,8 +84,24 @@ class BasicItemController {
     // model.addAttribute(item) 자동 추가
     @PostMapping("/add")
     public String addItemV4(Item item) {
-    itemRepository.save(item);
-    return "basic/item";
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    // 상품 수정 폼
+    // http://localhost:8080/basic/items/1/edit
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    //  상품 수정 처리 
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
     }
 
     // 테스트용 데이터 추가
